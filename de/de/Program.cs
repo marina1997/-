8107e37aeae08f6ff6cf54919c1b7b500412bae9
marina1997 <shemyakina.marina97@gmail.com>
2018-03-21 02:09:48 +0300

    Add files via upload

---
+++ de/de/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics;
+using Lachesis.QuantumComputing;
+
+
+namespace de
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deutsch deutsch = new Deutsch(new Random());
+            deutsch.Run();
+
+        }
+    }
+}
+
+class Deutsch
+{
+    protected Random Random;
+
+    public Deutsch(Random random)
+    {
+        this.Random = random;
+    }
+
+    public void Run()
+    {
+        int registerLength = 1; //кол-во кубитов. квантовый регистр это совокупность некоторого числа кубитов 
+
+        //Подготовка начально состояния
+        QuantumRegister inputRegister = new QuantumRegister(0, registerLength); // Входной регистр
+        QuantumRegister outputRegister = new QuantumRegister(1, registerLength);  // Выходной регистр 
+        QuantumRegister fullRegister = new QuantumRegister(inputRegister, outputRegister); // Полный регистр (получается тензорным произведением)
+        //Console.WriteLine(inputRegister);
+        //Console.WriteLine(outputRegister);
+        //Console.WriteLine(fullRegister);
+
+        fullRegister = QuantumGate.HadamardGateOfLength(2) * fullRegister; // преобразование адамара
+        Matrix<Complex> matrixOracle = MatrixOracle(registerLength); //Проектирование оракула
+        QuantumGate GateOracle = new QuantumGate(matrixOracle);//Получаем гейт (на основе matrixOracle)
+        fullRegister = GateOracle * fullRegister;
+        fullRegister = QuantumGate.HadamardGateOfLength(2) * fullRegister; // преобразование адамара
+        //Console.WriteLine(fullRegister);
+
+        fullRegister.Collapse(this.Random);//измерение (наблюление) первого регистра
+        int inputRegesterValue = fullRegister.GetValue(0, registerLength);
+        FunctionDefinition(inputRegesterValue);
+
+        Console.ReadKey();
+    }
+
+    protected static Matrix<Complex> MatrixOracle(int registerLength)
+    {
+        int matrixSize = 1 << (registerLength * 2);//получаем размер оракула(матрицы); в данном случае регистр равен 1. следовательно 1 << 2=4; Мы передали методу четверку
+        Matrix<Complex> matrixOracle = Matrix<Complex>.Build.Sparse(matrixSize, matrixSize); // создание разреженной матрицы. Разрежённая матрица — это матрица с преимущественно нулевыми элементами.  
+
+        //заполнение матрицы. f(x)=1 функция для которой я реализую оракул
+        // 0 1 0 0
+        // 1 0 0 0
+        // 0 0 0 1
+        // 0 0 1 0
+        matrixOracle.At(1, 0, 1);
+        matrixOracle.At(0, 1, 1);
+        matrixOracle.At(3, 2, 1);
+        matrixOracle.At(2, 3, 1);
+
+
+        return matrixOracle;
+    }
+
+    public void FunctionDefinition(int i)
+    {
+        if (i == 0) Console.WriteLine("Функция f константа");
+        else Console.WriteLine("Функция f сбалансирована");
+    }
+}
---
+++ de/de/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("de")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("de")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("5d12e20d-617d-4056-a1d3-aa3c5acbbf58")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
